47104cba18b5753a74b04a4f0a4a607b649ecbb4
Neetha Francis <neethafrancis26@gmail.com>
2024-06-12 21:09:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar/navbar";
+import Intro from "./components/Intro/intro";
+import About from "./components/About/about";
+import Footer from "./components/Footer/footer";
+import Team from "./components/Team/team";
+import Members from "./components/Members/members";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Intro />
+      <About />
+      <Team />
+      <Members />
+      <Footer />
     </div>
   );
 }
